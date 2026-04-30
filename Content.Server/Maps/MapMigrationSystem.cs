eb812db496dd143da3d80654438502a4e435aa59
thetuerk <46725294+ThanosDeGraf@users.noreply.github.com>
2026-05-01 01:49:34 +0200

    Added Aurum Migrations (#1)
    Please use `aurum_migration.yml` to remove or replace old mapped entities from maps!

--- Content.Server/Maps/MapMigrationSystem.cs
+++ Content.Server/Maps/MapMigrationSystem.cs
@@ -21,7 +21,7 @@ public sealed class MapMigrationSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _protoMan = default!;
     [Dependency] private readonly IResourceManager _resMan = default!;
 
-    private static readonly string[] MigrationFiles = { "/migration.yml", "/nf_migration.yml", "/mono_migration.yml" }; // Monolith: custom migration file
+    private static readonly string[] MigrationFiles = { "/migration.yml", "/nf_migration.yml", "/mono_migration.yml", "/aurum_migration.yml" }; // Monolith: custom migration file // Aurum
 
     public override void Initialize()
     {
